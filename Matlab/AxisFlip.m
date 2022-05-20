87ccb85af192b2f0f2c82f22aa3140ec21df97c6
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2022-05-20 12:30:26 +0900

    Update AxisFlip.m

--- Matlab/AxisFlip.m
+++ Matlab/AxisFlip.m
@@ -1,6 +1,5 @@
 function pose_new = AxisFlip(axis,pose_b)
-%AXISFLIP 축 뒤집기 python 코드 디버그 하면서 체크 사이즈랑 어디어디 뒤집어야하나...
-% 3x4 받는건가?? R에만 곱하나 아니면 t에도????
+%AXISFLIP axis flip
 %   pose (3,4)
 %   axis 
 %        [x y z]
@@ -16,4 +15,3 @@ pose_new = zeros(3,4);
 pose_new(:,1:3)= R_new;
 pose_new(:,4) = t_new
 end
-
